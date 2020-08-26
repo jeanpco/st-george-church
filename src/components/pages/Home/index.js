208a6ccf78615999efe79b789b7a8a2814b24e3b
Romain Penaruiz <rom.penaruiz@gmail.com>
2020-08-26 17:07:41 -0400

    cleaned up my test

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,17 +1,7 @@
 import React from 'react'
-import LocaleContext from '../../../context/LocaleProvider'
 
 const IndexLayout = () => {
-  const lang = React.useContext(LocaleContext)
-  const i18n = lang.i18n[lang.locale]
-
-  let title = ''
-
-  if (i18n.locale === 'en-ca') {
-    return (title = 'English')
-  }
-
-  return <div>{title}</div>
+  return <h2>English</h2>
 }
 
 export default IndexLayout
