922f463704da7c6998d208b5d50ef6f3c3a17982
Romano78 <rom.penaruiz@gmail.com>
2020-09-22 17:56:42 -0400

    Fixed PR comments

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import Img from 'gatsby-image'
 import { WidthLimiterContainer } from '../Layout/styles'
@@ -21,12 +21,16 @@ import Text from '../Utilities/Text'
 import Icon from '~/components/Icon'
 
 const Gallery = ({ query }) => {
+  const [galleryState, setGalleryState] = useState([])
+
+  useEffect(() => {
+    setGalleryState(galleryImageArray)
+  }, [])
+
   const galleryImageArray = []
   query.content.map((info) => {
     galleryImageArray.push(info)
   })
-
-  const galleryImageLength = galleryImageArray.length
 
   return (
     <GalleryContainer>
@@ -34,101 +38,114 @@ const Gallery = ({ query }) => {
         <GalleryContentContainer>
           <Desktop>
             <GallerSliderHeader>
-              <Title type="heading2">{query?.title ? query.title : ''}</Title>
+              {query?.title ? (
+                <Title as="h2" type="heading2">
+                  {query.title}
+                </Title>
+              ) : (
+                ''
+              )}
               <Icon type="cross-des" />
             </GallerSliderHeader>
-            <GallerySliderContainer>
-              <GallerySliderContent
-                slidesToShow={3}
-                slidesToScroll={2}
-                infinite={true}
-              >
-                {query.content?.length > 0
-                  ? query.content.map((info) => {
-                      const sliderImage = info?.gallery_img?.localFile
-                        ?.childImageSharp?.fluid
-                        ? info.gallery_img.localFile.childImageSharp.fluid
-                        : ''
-                      return (
-                        <Fragment
-                          key={
-                            info?.gallery_img?.localFile?.childImageSharp?.id
-                              ? info.gallery_img.localFile.childImageSharp.id
-                              : ''
-                          }
-                        >
-                          <GalleryItemContent>
-                            <GallerySliderItemsContainer>
-                              <Img fluid={sliderImage} />
-                              <GallerSliderTitle>
-                                <Title as="h3" type="heading3">
-                                  {info.gallery_section_title.text
-                                    ? info.gallery_section_title.text
-                                    : ''}
-                                </Title>
-                              </GallerSliderTitle>
-                              <GallerySliderText>
-                                <Text type="smallText700">
-                                  {info?.gallery_section_text
-                                    ? info.gallery_section_text
-                                    : ''}
-                                </Text>
-                              </GallerySliderText>
-                            </GallerySliderItemsContainer>
-                            <GallerySliderIcon>
-                              <Icon type="flower" />
-                            </GallerySliderIcon>
-                          </GalleryItemContent>
-                        </Fragment>
-                      )
-                    })
-                  : ''}
-              </GallerySliderContent>
-            </GallerySliderContainer>
+            {query?.content?.length > 0 ? (
+              <GallerySliderContainer>
+                <GallerySliderContent
+                  slidesToShow={3}
+                  slidesToScroll={2}
+                  infinite={true}
+                >
+                  {query?.content
+                    ? query.content.map((info, index) => {
+                        const sliderImage = info?.gallery_img?.localFile
+                          ?.childImageSharp?.fluid
+                          ? info.gallery_img.localFile.childImageSharp.fluid
+                          : ''
+                        return (
+                          <Fragment key={index}>
+                            <GalleryItemContent>
+                              <GallerySliderItemsContainer>
+                                {sliderImage ? <Img fluid={sliderImage} /> : ''}
+                                <GallerSliderTitle>
+                                  {info?.gallery_section_title?.text ? (
+                                    <Title as="h3" type="heading3">
+                                      {info.gallery_section_title.text}
+                                    </Title>
+                                  ) : (
+                                    ''
+                                  )}
+                                </GallerSliderTitle>
+                                <GallerySliderText>
+                                  {info?.gallery_section_text ? (
+                                    <Text type="smallText700">
+                                      {info.gallery_section_text}
+                                    </Text>
+                                  ) : (
+                                    ''
+                                  )}
+                                </GallerySliderText>
+                              </GallerySliderItemsContainer>
+                              <GallerySliderIcon>
+                                <Icon type="flower" />
+                              </GallerySliderIcon>
+                            </GalleryItemContent>
+                          </Fragment>
+                        )
+                      })
+                    : ''}
+                </GallerySliderContent>
+              </GallerySliderContainer>
+            ) : (
+              ''
+            )}
           </Desktop>
 
           <Tablet>
             <GallerSliderHeader>
-              <Title type="heading2">{query?.title ? query.title : ''}</Title>
+              {query?.title ? <Title type="heading2">{query.title}</Title> : ''}
               <Icon type="cross" />
             </GallerSliderHeader>
             <GallerySliderContainer>
               <GallerySliderContent
                 slidesToShow={1.4}
                 slidesToScroll={1}
-                galleryImageLength={galleryImageLength}
+                galleryImageLength={galleryState.length}
                 infinite={false}
               >
                 {query.content?.length > 0
-                  ? query.content.map((info) => {
+                  ? query.content.map((info, index) => {
                       const sliderImage = info?.gallery_img?.localFile
                         ?.childImageSharp?.fluid
                         ? info.gallery_img.localFile.childImageSharp.fluid
                         : ''
                       return (
-                        <Fragment
-                          key={
-                            info?.gallery_img?.localFile?.childImageSharp?.id
-                              ? info.gallery_img.localFile.childImageSharp.id
-                              : ''
-                          }
-                        >
+                        <Fragment key={index}>
                           <GallerySliderItemsContainer>
-                            <Img
-                              fluid={sliderImage}
-                              alt="photo gallery slider image"
-                            />
+                            {sliderImage ? (
+                              <Img
+                                fluid={sliderImage}
+                                alt="photo gallery slider image"
+                              />
+                            ) : (
+                              ''
+                            )}
+
                             <GallerSliderTitle>
-                              <Title as="h3" type="heading3">
-                                {info.gallery_section_title.text
-                                  ? info.gallery_section_title.text
-                                  : ''}
-                              </Title>
+                              {info.gallery_section_title.text ? (
+                                <Title as="h3" type="heading3">
+                                  {info.gallery_section_title.text}
+                                </Title>
+                              ) : (
+                                ''
+                              )}
                             </GallerSliderTitle>
                             <GallerySliderText>
-                              <Text type="smallText700">
-                                {info.gallery_section_text}
-                              </Text>
+                              {info?.gallery_section_text ? (
+                                <Text type="smallText700">
+                                  {info.gallery_section_text}
+                                </Text>
+                              ) : (
+                                ''
+                              )}
                             </GallerySliderText>
                           </GallerySliderItemsContainer>
                           <GallerySliderIcon>
