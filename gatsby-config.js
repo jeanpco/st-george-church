a634b6a0c2b950420f25ed8f5b25fbf3c3111668
Romano78 <rom.penaruiz@gmail.com>
2020-12-10 14:06:00 -0500

    pre-loaded fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,7 @@ module.exports = {
     'gatsby-plugin-page-transitions',
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-preload-fonts`,
     {
       resolve: 'gatsby-source-prismic',
       options: {
