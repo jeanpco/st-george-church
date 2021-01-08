c73bd6e60d02aab55ee3f827006abfdd1a563167
Field Office Design + Digital <jp@field-office.ca>
2021-01-08 14:55:14 -0500

    add details to email subject line

---
+++ src/lambda/email.js
@@ -0,0 +1,38 @@
+const sgMail = require('@sendgrid/mail')
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+exports.handler = function (event, context, callback) {
+  const body = JSON.parse(event.body)
+
+  const msg = {
+    to: body.to,
+    from: body.from,
+    subject: `Email from St-George Contact form: ${body.subject}`,
+    text: body.text,
+  }
+
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+  sgMail.send(msg).then(
+    () => {
+      callback(null, {
+        statusCode: 200,
+        body: 'success',
+      })
+    },
+    (error) => {
+      console.error(error)
+
+      if (error.response) {
+        console.error(error.response.body)
+
+        callback(null, {
+          statusCode: 400,
+          body: JSON.stringify(error.response.body),
+        })
+      }
+    }
+  )
+}
