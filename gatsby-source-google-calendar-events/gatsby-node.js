817345ee5f8714a53d0744453c628b4e107f714d
Field Office Design + Digital <jp@field-office.ca>
2021-01-11 15:23:34 -0500

    attempt fix technique from github

--- gatsby-source-google-calendar-events/gatsby-node.js
+++ gatsby-source-google-calendar-events/gatsby-node.js
@@ -77,7 +77,7 @@ exports.sourceNodes = async ({ actions }, options = defaultOptions) => {
     type: 'service_account',
     project_id: 'field-office-guessjs',
     private_key_id: process.env.PRIVATE_KEY_ID,
-    private_key: process.env.PRIVATE_KEY,
+    private_key: process.env.PRIVATE_KEY.replace(/\\n/gm, '\n'),
     client_email: process.env.CLIENT_EMAIL,
     client_id: process.env.CLIENT_ID,
     auth_uri: 'https://accounts.google.com/o/oauth2/auth',
